62234df36783cd9ac9f4fcb5bfa63cc1fecd5d01
Domin-Dev <domin.gamedev@gmail.com>
2025-10-03 13:16:28 +0200

    0.1262

--- Assets/Scripts/Horse.cs
+++ Assets/Scripts/Horse.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class Horse : MonoBehaviour
+public class   Horse : MonoBehaviour
 {
-    [SerializeField] public float speed;
+     [SerializeField] public  float speed;
     [SerializeField] public Transform riderPoint;
     public SortingGroup sortingGroup;
     private void Start()
     {
         sortingGroup = GetComponent<SortingGroup>();
+        float ko = Time.time;
     }
+    //   flos
 }
 
